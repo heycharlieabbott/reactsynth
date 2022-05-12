4dc57369bda4e7ab20bbbdc5ee260f866181ab40
heycharlieabbott <chzabbott@gmail.com>
2022-05-13 01:30:14 +0900

    fix memory leak

--- src/osc1.js
+++ src/osc1.js
@@ -1,23 +1,18 @@
 import React from "react";
 import * as Tone from 'tone';
 
+const synth = new Tone.AMSynth().connect(Tone.Destination);
+//const synth2 = new Tone.AMSynth().connect(props.output);
+
 
 
 export default function Testo(props){
-    // const eyy = [props.sound];
-    // const yo = eyy.map((item) => {
-    //     return item  + Math.random();
-    // })
 
 
 
-const synth = new Tone.AMSynth().connect(props.output);
-const synth2 = new Tone.AMSynth().connect(props.output);
+
 
     function playSynth(){
-        // synth.triggerAttackRelease(3000 * Math.random(),.01);
-        
-        // setTimeout( () => synth2.triggerAttackRelease(2000  * Math.random(),.01),500);
 
         synth.triggerAttackRelease(props.freq,0.1);
 
--- src/slider.js
+++ src/slider.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import Chain from "./chain";
 ;
 
@@ -8,10 +8,27 @@ export default function Slider(props){
 
     const [sliderval, setSliderVal] = useState(500);
 
+    
     function slide(event){
         setSliderVal(() => event.target.value);
+        //console.log(sliderval)
         
     }
+
+    const sliref = useRef(500);
+
+    function slide2(event){
+
+    
+ 
+    sliref.current = event.target.value;
+    
+
+    console.log(sliref.current);
+
+    }
+
+
 
     
     
@@ -19,8 +36,9 @@ export default function Slider(props){
     return(
         <div>
         
+        
+        <input type="range" min="200" max="1000" onChange={slide} class="slider"></input>
         <Chain vol={sliderval}/>
-        <input type="range" min="200" max="1000" value={sliderval} onChange={slide} class="slider"></input>
        
 
            
